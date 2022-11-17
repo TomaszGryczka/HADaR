3d32feeeb524dc9b840d3612c46b47a30a0cb549
TomaszGryczka <tumek001@gmail.com>
2022-11-17 18:37:30 +0100

    initial spring boot project

---
+++ src/test/java/com/hadar/hadar/HadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadar.hadar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
